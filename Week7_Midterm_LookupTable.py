c722ebca2f6abcc359ef040f2b79d9f15770fac4
Featherxu <featherxu626@gmail.com>
2021-07-01 12:46:07 +0800

    Create Week7_Midterm_LookupTable.py

---
+++ Week7_Midterm_LookupTable.py
@@ -0,0 +1,104 @@
+# import modules
+import board
+import time
+import analogio
+import neopixel
+from simpleio import map_range
+import adafruit_fancyled.adafruit_fancyled as fancy
+import rotaryio
+
+
+# declare objects
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
+
+# declare analong input
+#pot = analogio.AnalogIn(board.A6)
+encoder = rotaryio.IncrementalEncoder(board.A2, board.A3)
+photo = analogio.AnalogIn(board.A1)
+
+NUM_LED = 10
+my_colors = {0: (255,125,0),
+          1: (255,138,25),
+          2: (255,151,50),
+          3: (255,164,75),
+          4: (255,177,100),
+          5: (255,190,125),
+          6: (255,203,150),
+          7: (255,216,175),
+          8: (255,229,200),
+          9: (255,242,225),
+
+          10: (255,255,255),
+
+          11: (225,242,255),
+          12: (200,229,255),
+          13: (175,216,255),
+          14: (150,203,255),
+          15: (125,190,255),
+          16: (100,77,255),
+          17: (75,164,255),
+          18: (50,151,255),
+          19: (25,138,255),
+          20: (0,125,255),
+
+          }
+
+
+#color = (0,0,0)
+POSMIN = 0
+POSMAX = 21
+
+brightness = photo.value
+
+count = encoder.position
+color = my_colors[encoder.position]
+# declare color variable & fill the pixels with the colors
+encoder.position = 10
+
+
+# repeat this code 4eva
+while True:
+
+    encoder.position = 10
+    RED = (255, 0, 0)
+
+    for x in range(encoder.position):
+        #if count = encoder.position:
+        pixels.fill(count)
+
+        if encoder.position != encoder.position:
+            pixels.fill(color)
+
+        #for x in range(NUM_LED):
+            # pixels[20-x] = (0, 0, 0)
+           # pixels[x] = (
+
+
+
+    if count<=POSMIN:
+        count = POSMIN
+    elif count>=POSMAX:
+        count = POSMAX
+
+    print(color)
+
+    # bit shift scale 16-bit to 8-bit value
+    #scaled_hue = map_range(hue, 0, 65535, 0, 255)
+    scaled_hue = encoder.position
+    #scaled_hue = int(scaled_hue)
+    #print("hue=", scaled_hue)
+
+    #scaled_bright = map_range(brightness, 20000, 55535, 0, 255)
+    #scaled_bright = int(scaled_bright)
+    scaled_bright = 200
+    print("brightness=", scaled_bright)
+
+
+    color = fancy.CHSV(encoder.position, 200, scaled_bright)
+    packed = color.pack()
+
+    #color = (0, scaled_pot, scaled_photo)
+
+    pixels.fill(packed)
+
+    time.sleep(0.1)
